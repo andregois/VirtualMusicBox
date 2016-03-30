0c8d99cf9761a32236b5866fca6644eca25f6d23
Pablo Victor <pablo.santos@ccc.ufcg.edu.br>
2016-03-29 22:00:08 -0300

    Leitura do QR Code

---
+++ app/src/main/java/com/ufcg/virtualmusicbox/LogonActivity.java
@@ -0,0 +1,50 @@
+package com.ufcg.virtualmusicbox;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class LogonActivity extends CaptureActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logon);
+        ImageView imageViewQRCode = (ImageView) findViewById(R.id.imageView);
+        imageViewQRCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(LogonActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Escaneamento do QR Code");
+                integrator.setCaptureActivity(LogonActivity.class);
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Qr Code escaneado: " + result.getContents(), Toast.LENGTH_LONG).show();
+                startActivity(new Intent(this, MainActivity.class));
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
